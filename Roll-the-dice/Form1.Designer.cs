77aac5a55d85cae105c28f838c9a72c2cd0d8bb2
JonathasGomes <jonathas553@hotmail.com>
2022-04-02 13:47:20 -0300

    feature: images and components

--- Roll-the-dice/Form1.Designer.cs
+++ Roll-the-dice/Form1.Designer.cs
@@ -29,13 +29,49 @@ namespace Roll_the_dice
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbDice_1 = new System.Windows.Forms.PictureBox();
+            this.pbDice_2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice_2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbDice_1
+            // 
+            this.pbDice_1.Location = new System.Drawing.Point(228, 104);
+            this.pbDice_1.Name = "pbDice_1";
+            this.pbDice_1.Size = new System.Drawing.Size(102, 98);
+            this.pbDice_1.TabIndex = 0;
+            this.pbDice_1.TabStop = false;
+            // 
+            // pbDice_2
+            // 
+            this.pbDice_2.Location = new System.Drawing.Point(472, 104);
+            this.pbDice_2.Name = "pbDice_2";
+            this.pbDice_2.Size = new System.Drawing.Size(102, 98);
+            this.pbDice_2.TabIndex = 1;
+            this.pbDice_2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbDice_2);
+            this.Controls.Add(this.pbDice_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbDice_2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbDice_1;
+        private System.Windows.Forms.PictureBox pbDice_2;
     }
 }
 
